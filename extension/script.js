9ec6bbdeaec6f622d2227a86cf9d2404a0492c7c
Jacob Petersen <jacob@jacob-petersen.com>
2018-11-13 20:12:07 -0600

    Getting extension to work on first load

--- extension/script.js
+++ extension/script.js
@@ -1,23 +1,24 @@
 const currentTime = (new Date()).getTime();
 const getImgsFile = (url, callback) => {
-  chrome.storage.sync.get(['lastLoaded'], function(result) {
-    if(result.lastLoaded - currentTime > 86400000) {
+  chrome.storage.sync.get(['lastLoaded', 'landscapeLinks', 'portraitLinks', 'index'], function(result) {
+    if((result.lastLoaded && result.lastLoaded - currentTime > 86400000) || !result.lastLoaded) {
       const xhr = new XMLHttpRequest();
       xhr.open('GET', url);
       xhr.onload = function() {
         if (xhr.status === 200) {
           const imgLinks = JSON.parse(xhr.responseText);
-          chrome.storage.sync.set({
+          const newStorageVals = {
             lastLoaded: currentTime,
             landscapeLinks: imgLinks['landscape'],
             portraitLinks: imgLinks['portrait'],
             index: 0,
-          }, callback());
+          };
+          chrome.storage.sync.set(newStorageVals, callback(newStorageVals));
         }
       };
       xhr.send();
     } else {
-      callback();
+      callback(result);
     }
   });
 }
@@ -37,34 +38,33 @@ const setPhotoCredit = (img) => {
   nameSpan.innerHTML = img.name;
   photoCreditSection.style.display = 'inline'
 }
-const loadNewImg = () => {
-  chrome.storage.sync.get(['landscapeLinks', 'portraitLinks', 'index'], function(result) {
-    const orientation = getScreenOrientation();
-    const img = orientation === 'landscape' ? result['landscapeLinks'][result['index']] : result['portraitLinks'][result['index']];
-    if(orientation === 'landscape') {
-      img.url += '&w=1000'
-    } else {
-      img.url += '&h=1000'
-    }
-    const photoElement = document.getElementById('photo');
-    photoElement.style.backgroundImage = 'url("' + img.url + '")';
-    photoElement.setAttribute('aria-label', img.description);
-    setPhotoCredit(img);
-  });
+const loadNewImg = (newStorageVals) => {
+  const orientation = getScreenOrientation();
+  const img = orientation === 'landscape' ? newStorageVals['landscapeLinks'][newStorageVals['index']] : newStorageVals['portraitLinks'][newStorageVals['index']];
+  if(orientation === 'landscape') {
+    img.url += '&w=1000'
+  } else {
+    img.url += '&h=1000'
+  }
+  const photoElement = document.getElementById('photo');
+  photoElement.style.backgroundImage = 'url("' + img.url + '")';
+  photoElement.setAttribute('aria-label', img.description);
+  setPhotoCredit(img);
 }
 const updateIndex = () => {
-  chrome.storage.sync.get(['index'], function(result) {
+  chrome.storage.sync.get(['lastLoaded', 'landscapeLinks', 'portraitLinks', 'index'], function(result) {
     let newIndex = result.index + 1;
     if(newIndex > 29) {
       newIndex = 0;
     }
+    result.index = newIndex;
     chrome.storage.sync.set({
       index: newIndex,
-    }, loadNewImg());
+    }, loadNewImg(result));
   });
 }
-const setup = () => {
-  loadNewImg();
+const setup = (setValues) => {
+  loadNewImg(setValues);
   document.getElementById('reload').addEventListener('click', updateIndex);
 }
 getImgsFile('https://streetarttab.com/unsplashLinks.json', setup);
